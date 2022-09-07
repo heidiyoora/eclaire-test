c8f62c00917c1a96179573503e627a3c8c915c71
Dan Fyfe <fyfedaniel@gmail.com>
2022-09-07 15:56:01 -0400

    update comment

--- my-project/src/api/page/controllers/page.js
+++ my-project/src/api/page/controllers/page.js
@@ -8,12 +8,13 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::page.page', ({strapi}) => ({
   async find(ctx) {
+    // api/pages?filters[url][$eq]=URL
     const { query } = ctx;
 
     const entity = await super.find({ 
       ...ctx, 
       query: { ...query, populate: [
-        // create populate query with page and content specific
+        // create populate query for page and content specific data
         // will probably need to add a few more
         'socialImage', 'contents.modules.image',
         'contents.modules.elements', 'contents.modules.products'
