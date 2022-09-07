04856a6b6b49787edb351d043cc19fd6648be741
Dan Fyfe <fyfedaniel@gmail.com>
2022-09-07 15:26:11 -0400

    remove versioning (plugin still in there, but functionality gone), add content collection type, controller testing

--- my-project/src/api/page/controllers/page.js
+++ my-project/src/api/page/controllers/page.js
@@ -6,4 +6,20 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::page.page');
+module.exports = createCoreController('api::page.page', ({strapi}) => ({
+  async find(ctx) {
+    const { query } = ctx;
+
+    const entity = await super.find({ 
+      ...ctx, 
+      query: { ...query, populate: [
+        // create populate query with page and content specific
+        // will probably need to add a few more
+        'socialImage', 'contents.modules.image',
+        'contents.modules.elements', 'contents.modules.products'
+      ] }
+    });
+
+    return entity;
+  }
+}));
