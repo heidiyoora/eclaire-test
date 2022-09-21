f487f14a8c3931ce80d943f898c40ca3c28e0128
Qamar <qazam@lucidya.com>
2022-09-21 17:33:49 +0300

    create content on page update

--- my-project/src/api/page/services/page.js
+++ my-project/src/api/page/services/page.js
@@ -19,11 +19,16 @@ module.exports = createCoreService("api::page.page", ({ strapi }) => ({
   async updateContent(contents, id) {
     return await Promise.all(
       contents.map(async (content) => {
-        return await strapi.entityService.update(
-          "api::content.content",
-          content.id,
-          { data: { ...content, page: id } }
-        );
+        // check if content already exist then update otherwise create it.
+        return content.id
+          ? await strapi.entityService.update(
+              "api::content.content",
+              content.id,
+              { data: { ...content, page: id } }
+            )
+          : await strapi.entityService.create("api::content.content", {
+              data: { ...content, page: id },
+            });
       })
     );
   },
