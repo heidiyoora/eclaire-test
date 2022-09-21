48e714d31567f01411db12968086f57090983c49
Qamar <qazam@lucidya.com>
2022-09-21 16:44:47 +0300

    fix cron job

--- my-project/config/cron-tasks.js
+++ my-project/config/cron-tasks.js
@@ -1,14 +1,20 @@
 module.exports = {
   myJob: {
     task: async () => {
-      // fetch uncoupled content
+      console.log(new Date());
+
+      // fetch uncouple content
       const contents = await strapi.db.query("api::content.content").findMany({
         where: {
           page: null,
+          updatedAt: {
+            // check page updated 7 days before
+            $lt: new Date(new Date() - 7 * 24 * 60 * 60 * 1000),
+          },
         },
       });
 
-      if (contents.length) {
+      if (contents?.length) {
         // collect content ids in order to delete in bulk
         const contentIds = contents.map((content) => content.id);
 
@@ -19,7 +25,7 @@ module.exports = {
       }
     },
     options: {
-      rule: "* * * * */0",
+      rule: "* */9 * * *",
       tz: "Europe/Istanbul",
     },
   },
