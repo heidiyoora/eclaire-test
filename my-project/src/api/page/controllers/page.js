2c533fd248b53a6bda7caecdb31b9d5db5010ff6
Dan Fyfe <fyfedaniel@gmail.com>
2022-09-21 10:41:00 -0400

    start comments/questions

--- my-project/src/api/page/controllers/page.js
+++ my-project/src/api/page/controllers/page.js
@@ -32,11 +32,14 @@ module.exports = createCoreController("api::page.page", ({ strapi }) => ({
   },
   async createPage(ctx) {
     const { page_data, content_data } = ctx.request.body;
+    console.log('page_data: ', page_data)
     try {
       const page = await strapi.entityService.create("api::page.page", {
         data: page_data,
       });
-
+      /// do we need to create content when a page is created?
+      /// we currently do not allow for that in the UI
+      /// if we are not updating that, we can probably remove this?
       if (page && page.id && content_data?.length) {
         // create multiple contents and associate with page
         const contents = await strapi
@@ -45,28 +48,40 @@ module.exports = createCoreController("api::page.page", ({ strapi }) => ({
         return { page, contents };
       }
       return page;
-    } catch {
-      ctx.throw(500, err);
+    } catch (error) {
+      ctx.throw(500, error);
     }
   },
   async updatePage(ctx) {
     const { id } = ctx.request.params;
-    const { page_data, content_data } = ctx.request.body;
+    const { page_data = null, content_data } = ctx.request.body;
     try {
-      const page = await strapi.entityService.update("api::page.page", id, {
-        data: page_data,
+      // we will have situations where page data is not updated, so we will have to
+      // account for no page data
+      let page;
+      if (page_data) {
+        page = await strapi.entityService.update("api::page.page", id, {
+          data: page_data,
+        });
+      } else {
+        page = await strapi.entityService.findOne("api::page.page", id, { 
+            populate: ['contents.modules']
       });
-
+      }
+      // more of a check here? - maybe check for content id, since its an update
       if (id && content_data?.length) {
         // update contents
         const contents = await strapi
           .service("api::page.page")
           .updateContent(content_data, id);
-        return { page, contents };
+        // return { page, contents };
       }
+      // how can we return all of this data in one place
+      // if we go this route, the 'page' returned here is out of date
+      // we would have to either have to refetch data or maybe move the content updates first?
       return page;
-    } catch {
-      ctx.throw(500, err);
+    } catch (error) {
+      ctx.throw(500, error);
     }
   },
   async deletePage(ctx) {
@@ -74,8 +89,8 @@ module.exports = createCoreController("api::page.page", ({ strapi }) => ({
     try {
       const page = await strapi.entityService.delete("api::page.page", id);
       return page;
-    } catch {
-      ctx.throw(500, err);
+    } catch (error) {
+      ctx.throw(500, error);
     }
   },
 }));
